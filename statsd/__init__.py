28c51771f48fbcc78cbafb83717a518b39aca036
Thurloat <thurloat@gmail.com>
2011-11-29 19:57:29 -0400

    Should probably include it in the module

--- statsd/__init__.py
+++ statsd/__init__.py
@@ -1,7 +1,8 @@
 from statsd.connection import Connection
 from statsd.client import Client
 from statsd.timer import Timer
+from statsd.raw import Raw, raw, discrete
 from statsd.counter import Counter, increment, decrement
 
-__all__ = ['Client', 'Connection', 'Timer', 'Counter', 'increment', 'decrement']
+__all__ = ['Client', 'Connection', 'Timer', 'Counter', 'increment', 'decrement', 'Raw', 'raw', 'discrete']
 
